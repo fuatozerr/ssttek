9cc0e3c7712de465a0f0d23e9364177e0ddac6b5
Fuat <fuatozerr23@gmail.com>
2022-06-22 18:48:01 +0300

    adding reddis settings

---
+++ ssttek/ssttek.Contact.Redis/Settings/RedisSettings.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+namespace ssttek.Contact.Redis.Settings
+{
+    public class RedisSettings
+    {
+        private readonly string _host;
+
+        private readonly int _port;
+
+        private ConnectionMultiplexer _ConnectionMultiplexer;
+
+        public RedisSettings(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Connect() => _ConnectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+
+        public IDatabase GetDb(int db = 1) => _ConnectionMultiplexer.GetDatabase(db);
+    }
+}
