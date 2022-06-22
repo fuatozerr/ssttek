f68510d59f4d4ca894e97823e2ca8ebf3a7db15a
Fuat <fuatozerr23@gmail.com>
2022-06-22 18:40:40 +0300

    adding model

--- ssttek/ssttek.Contact.Redis/Dtos/Contact.cs
+++ ssttek/ssttek.Contact.Redis/Dtos/Contact.cs
@@ -2,5 +2,10 @@
 {
     public class Contact
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Firm { get; set; }
+        public string ContactInformation { get; set; } //lokasyon
     }
 }
