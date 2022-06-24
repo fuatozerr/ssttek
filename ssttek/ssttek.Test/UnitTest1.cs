7b1f914f65f42ed536ad6ab014d0c59f0d2fe4e4
Fuat <fuatozerr23@gmail.com>
2022-06-24 18:32:36 +0300

    adding test metot

--- ssttek/ssttek.Test/UnitTest1.cs
+++ ssttek/ssttek.Test/UnitTest1.cs
@@ -1,4 +1,8 @@
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ssttek.Contact.Redis.Controllers;
+using ssttrek.Business.Abstract;
 
 namespace ssttek.Test
 {
@@ -6,8 +10,16 @@ namespace ssttek.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public async void TestMethod1()
         {
+            var repository = new Mock<IContactService>();
+            var distributedCache = new Mock<IDistributedCache>();
+            repository.Setup(repo => repo.GetAllContacts());
+            var controller = new ContactController(repository.Object, distributedCache.Object);
+
+            var result = await controller.GetAllContact();
+            Assert.IsNotNull(result);
+
         }
     }
 }
